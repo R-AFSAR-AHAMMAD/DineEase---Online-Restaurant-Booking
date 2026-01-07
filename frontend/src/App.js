66f573d75c516e8e5705120bb5241141f804906e
R-AFSAR-AHAMMAD <afsarahmed592@gmail.com>
2026-01-08 00:35:33 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css"
+import Navbar from './components/Navbar'
+const App = () => {
+  return (
+    <main className='main-container'>
+      <Navbar />
+    </main>
+  )
+}
+
+export default App
---
+++ frontend/src/components/Navbar/index.js
@@ -0,0 +1,20 @@
+import "./index.css";
+
+const Navbar = () => {
+  return (
+      <nav className="nav-container">
+        <h1 className="logo">DineEase</h1>
+
+        <ul className="nav-links">
+          <li>Home</li>
+          <li>Menu</li>
+          <li>Reservation</li>
+          <li>About</li>
+          <li>Team</li>
+        </ul>
+        <button className="our-menu-btn">Our Menu</button>
+      </nav>
+  );
+};
+
+export default Navbar;
